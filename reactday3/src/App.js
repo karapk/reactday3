0b85915db387919842023b156c0dd7cb68126069
Philip Karanja <kpk.my.mailings@gmail.com>
2024-06-03 02:02:06 -0500

    useEffect removed

--- reactday3/src/App.js
+++ reactday3/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import MenuItem from './components/MenuItem';
 import { fakeMenuItems, fakeMerchItems } from './fakeData';
@@ -7,8 +7,8 @@ import Footer from './components/Footer';
 
 function App() {
 
-  const [menuItems, setMenuItems] = useState(fakeMenuItems)
-  const [merchItems, setMerchItems] = useState(fakeMerchItems)
+  const [menuItems] = useState(fakeMenuItems)
+  const [merchItems] = useState(fakeMerchItems)
   
   return (
    <div className='container'>
